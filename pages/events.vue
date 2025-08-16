cf2441fcd6e70c393dcc9a58bd4a965598384d45
Njenga <petermuthuinjenga@gmail.com>
2025-08-16 10:09:01 +0300

    blank pages added

---
+++ pages/events.vue
@@ -0,0 +1,9 @@
+<template>
+  <TopContainer :image="Team1" alt="Events" title="Events" class="mv-image" />
+</template>
+
+<script setup lang="ts">
+import Team1 from "@/assets/team1.jpg";
+</script>
+
+<style scoped></style>
---
+++ pages/shop.vue
@@ -0,0 +1,14 @@
+<template>
+  <TopContainer
+    :image="team2"
+    alt="About Us"
+    title="About Us"
+    class="mv-image"
+  />
+</template>
+
+<script setup lang="ts">
+import team2 from "@/assets/team2.jpg";
+</script>
+
+<style scoped></style>
